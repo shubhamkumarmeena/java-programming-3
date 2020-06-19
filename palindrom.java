2166dd9b5fd6a20eede938e0e0d70fd2857978a8
shubhamkumarmeena <sk8656933@gmail.com>
2020-06-19 20:27:39 +0530

    java-programming-3

---
+++ palindrom.java
@@ -0,0 +1,41 @@
+
+package testing;
+import java.util.Scanner;
+
+public class palindrom {
+
+    public static void main(String args[]){
+
+        System.out.println("Please Enter a number : ");
+        int palindrome = new Scanner(System.in).nextInt();
+
+        if(isPalindrome(palindrome)){
+            System.out.println("Number : " + palindrome + " is a palindrome");
+        }else{
+            System.out.println("Number : " + palindrome + " is not a palindrome");
+        }
+
+    }
+
+
+    public static boolean isPalindrome(int number) {
+        int palindrome = number; // copied number into variable
+        int reverse = 0;
+
+        while (palindrome != 0) {
+            int remainder = palindrome % 10;
+            reverse = reverse * 10 + remainder;
+            palindrome = palindrome / 10;
+        }
+
+
+        if (number == reverse) {
+            return true;
+        }
+        return false;
+    }
+
+}
+
+
+
